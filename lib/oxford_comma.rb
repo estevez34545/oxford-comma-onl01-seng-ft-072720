28404b75a23bf967f88fedffeaf0f5c1bd161e63
lisbeth estevez <estevez34545@gmail.com>
2020-08-01 00:08:20 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -8,3 +8,8 @@ def oxford_comma(array)
         array.join
     end
 end
+def oxford_comma(list)
+  return list.join(' and ') if list.size < 3
+  list[-1] = "and " + list[-1]
+  list.join(', ')
+end
