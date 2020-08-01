01667448b4bee3d854ffbb312349243e67afdc7c
lisbeth estevez <estevez34545@gmail.com>
2020-08-01 00:08:36 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+    if array.length == 2
+        array.join(" and ")
+    elsif array.length > 2
+        array.insert(-2, "and")
+        array[0..-1].join(", ")
+    else
+        array.join
+    end
+end
+def oxford_comma(list)
+  return list.join(' and ') if list.size < 3
+  list[-1] = "and " + list[-1]
+  list.join(', ')
 end
